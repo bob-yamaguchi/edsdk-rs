a1c70c46a9d764667034a19135fd4d239a2a2a43
bob-yamaguchi <bob.yamaguchi@gmail.com>
2021-06-02 03:37:01 +0900

    modify settings

--- build.rs
+++ build.rs
@@ -3,7 +3,8 @@ use std::fs;
 use std::path::Path;
 
 fn main() {
-    println!("cargo:rustc-link-search=native=./lib");
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    println!("cargo:rustc-link-search=native={}/lib", manifest_dir);
 
     let out_dir = env::var("OUT_DIR").unwrap();
     let dest_dll_path = Path::new(&out_dir).join("../../..").join("EDSDK.dll");
